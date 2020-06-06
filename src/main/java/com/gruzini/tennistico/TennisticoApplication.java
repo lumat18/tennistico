0312cdcb6dd251753a8ec496c0b5b4ea71823395
lumat <lumat@op.pl>
2020-06-06 15:39:55 +0200

    init project

---
+++ src/main/java/com/gruzini/tennistico/TennisticoApplication.java
@@ -0,0 +1,13 @@
+package com.gruzini.tennistico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisticoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisticoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gruzini/tennistico/TennisticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruzini.tennistico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisticoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
