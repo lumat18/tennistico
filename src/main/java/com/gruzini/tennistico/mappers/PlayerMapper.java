b019c771c0172cd8882a27b25cdd5c2ff6832fbe
Grzegorz <serwacy2@gmail.com>
2020-06-06 21:04:51 +0200

    changes to player - playerDTO exchange

--- src/main/java/com/gruzini/tennistico/mappers/PlayerMapper.java
+++ src/main/java/com/gruzini/tennistico/mappers/PlayerMapper.java
@@ -4,6 +4,9 @@ import com.gruzini.tennistico.domain.Player;
 import com.gruzini.tennistico.models.PlayerDTO;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 @Component
 public class PlayerMapper {
     public Player mapPlayerDTOtoPlayer(final PlayerDTO playerDTO){
@@ -15,5 +18,21 @@ public class PlayerMapper {
                 .description(playerDTO.getDescription())
                 .playerSkill(playerDTO.getPlayerSkill())
                 .build();
+    }
+
+    public PlayerDTO mapPlayerToPlayerDTO(final Player player){
+        return PlayerDTO.builder()
+                .firstName(player.getFirstName())
+                .lastName(player.getLastName())
+                .gender(player.getGender())
+                .birthday(player.getBirthday())
+                .age(calculateAge(player.getBirthday()))
+                .description(player.getDescription())
+                .playerSkill(player.getPlayerSkill())
+                .build();
+    }
+
+    private Integer calculateAge(final LocalDate birthday){
+        return Period.between(birthday, LocalDate.now()).getYears();
     }
 }
