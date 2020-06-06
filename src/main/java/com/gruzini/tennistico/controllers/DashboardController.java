a0c634645a679ce76022634474658908c99094c5
Bartek <bart.matusz@gmail.com>
2020-06-06 21:38:15 +0200

    basic fields on dashboard layout proposal

---
+++ src/main/java/com/gruzini/tennistico/controllers/DashboardController.java
@@ -0,0 +1,15 @@
+package com.gruzini.tennistico.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @GetMapping
+    public String showDashboard(){
+        return "dashboard";
+    }
+}
