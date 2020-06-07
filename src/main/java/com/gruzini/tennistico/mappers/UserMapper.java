5e0596428c573f33c3b0cc0d506b602145c0c884
lumat <lumat@op.pl>
2020-06-07 16:12:27 +0200

    add registration mappers

---
+++ src/main/java/com/gruzini/tennistico/mappers/UserMapper.java
@@ -0,0 +1,28 @@
+package com.gruzini.tennistico.mappers;
+
+import com.gruzini.tennistico.domain.User;
+import com.gruzini.tennistico.enums.UserStatus;
+import com.gruzini.tennistico.forms.UserRegistrationForm;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class UserMapper {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserMapper(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User toUser(final UserRegistrationForm registrationForm) {
+        return User.builder()
+                .email(registrationForm.getEmail())
+                .password(passwordEncoder.encode(registrationForm.getPassword()))
+                .userStatus(UserStatus.INACTIVE)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
