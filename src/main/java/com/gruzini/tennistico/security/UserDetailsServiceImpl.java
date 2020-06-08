c7422bbd22384ec8ce6efc470924b0ab839a6880
Bartek <bart.matusz@gmail.com>
2020-06-08 21:57:33 +0200

    added test to check if correct view is returned

--- src/main/java/com/gruzini/tennistico/security/UserDetailsServiceImpl.java
+++ src/main/java/com/gruzini/tennistico/security/UserDetailsServiceImpl.java
@@ -6,12 +6,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserServiceDetailsImpl  implements UserDetailsService {
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserService userService;
 
-    public UserServiceDetailsImpl(UserService userService) {
+    public UserDetailsServiceImpl(UserService userService) {
         this.userService = userService;
     }
 
