56fdcfd750c17336545d86c3c84d6ae2d475b1e7
Bartek <bart.matusz@gmail.com>
2020-06-08 23:33:54 +0200

    secured endpoints except welcomePage, login, registration

--- src/main/java/com/gruzini/tennistico/security/SecurityConfig.java
+++ src/main/java/com/gruzini/tennistico/security/SecurityConfig.java
@@ -19,8 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
-//                .antMatchers("/**").authenticated()
+                .antMatchers("/", "/login", "/registration/**").permitAll()
+                .antMatchers("/**").authenticated()
                 .anyRequest()
                 .permitAll();
 
