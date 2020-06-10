8af1ded14c974e94f7e68a24bf0b54da6d782279
lumat <lumat@op.pl>
2020-06-10 22:58:58 +0200

    add /login-error to SecurityConfig

--- src/main/java/com/gruzini/tennistico/security/SecurityConfig.java
+++ src/main/java/com/gruzini/tennistico/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/login", "/registration/**", "/activate/**").permitAll()
+                .antMatchers("/", "/login", "/login-error", "/registration/**", "/activate/**").permitAll()
                 .antMatchers("/css/**", "/js/**", "/img/**").permitAll()
                 .antMatchers("/h2/**").permitAll()
                 .antMatchers("/**").authenticated()
