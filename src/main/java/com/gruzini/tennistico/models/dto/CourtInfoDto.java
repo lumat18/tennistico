14356c074bdf145241747a4b8f874ef55ab85ce5
Bartek <bart.matusz@gmail.com>
2020-06-12 18:16:52 +0200

    created CourtInfoDto

--- src/main/java/com/gruzini/tennistico/models/dto/CourtInfoDto.java
+++ src/main/java/com/gruzini/tennistico/models/dto/CourtInfoDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CourtInfoDto {
+    private Long courtId;
     private String name;
     private String address;
     private String city;
