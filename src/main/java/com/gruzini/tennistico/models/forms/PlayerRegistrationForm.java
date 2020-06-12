1a32970536769784ea4395fc280d901b52b71bf5
Grzegorz <serwacy2@gmail.com>
2020-06-12 18:15:20 +0200

    made every field in playerRegistrationForm mandatory with error handling

--- src/main/java/com/gruzini/tennistico/models/forms/PlayerRegistrationForm.java
+++ src/main/java/com/gruzini/tennistico/models/forms/PlayerRegistrationForm.java
@@ -31,15 +31,19 @@ public class PlayerRegistrationForm {
     @Length(min = 1, message = "Last name cannot be empty")
     private String lastName;
 
+    @NotNull(message = "You have to pick gender")
     private Gender gender;
 
+    @NotNull(message = "You have to provide your date of birth")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Past(message = "You cannot be born in future")
     private LocalDate birthday;
 
+    @NotNull(message = "You have to provide number of years you have played")
     @Min(value = 0, message = "You must provide a number ranging from 0 to 99")
     @Max(value = 99, message = "You must provide a number ranging from 0 to 99")
     private Integer yearsOfExperience;
 
+    @NotNull(message = "You have to pick your level of skill")
     private PlayerSkill playerSkill;
 }
