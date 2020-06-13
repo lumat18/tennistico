5c35bbefbbfffffafcd259524534544433d28015
lumat <lumat@op.pl>
2020-06-13 15:15:19 +0200

    create SchedulingConfig

--- src/main/java/com/gruzini/tennistico/TennisticoApplication.java
+++ src/main/java/com/gruzini/tennistico/TennisticoApplication.java
@@ -2,10 +2,8 @@ package com.gruzini.tennistico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TennisticoApplication {
 
     public static void main(String[] args) {
