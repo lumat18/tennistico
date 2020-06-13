a088d8106da7e8524abbb70c49ef741af37ffc0a
Bartek <bart.matusz@gmail.com>
2020-06-13 17:20:58 +0200

    added court initialization

---
+++ src/main/java/com/gruzini/tennistico/bootstrap/CourtInitializer.java
@@ -0,0 +1,50 @@
+package com.gruzini.tennistico.bootstrap;
+
+import com.gruzini.tennistico.domain.Court;
+import com.gruzini.tennistico.repositories.CourtRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourtInitializer implements CommandLineRunner {
+
+    private final CourtRepository courtRepository;
+
+    public CourtInitializer(CourtRepository courtRepository) {
+        this.courtRepository = courtRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        final Court court1 = Court.builder()
+                .phoneNumber("+33 555 222 111")
+                .zipCode("55-555")
+                .street("Key Biscayne")
+                .country("Florida")
+                .state("Florida")
+                .city("Miami")
+                .name("Crandon Park Tennis Center")
+                .build();
+        final Court court2 = Court.builder()
+                .phoneNumber("+351 54 781 781")
+                .zipCode("31-300")
+                .street("Rua do Benformoso")
+                .state("Portugal")
+                .country("Portugal")
+                .city("Lisbon")
+                .name("Sport Clube Intendente")
+                .build();
+        final Court court3 = Court.builder()
+                .phoneNumber("+00 500 500")
+                .zipCode("30-30")
+                .street("Long")
+                .country("Florida")
+                .state("Florida")
+                .city("Miami")
+                .name("Sunny Court Center")
+                .build();
+        courtRepository.save(court1);
+        courtRepository.save(court2);
+        courtRepository.save(court3);
+    }
+}
