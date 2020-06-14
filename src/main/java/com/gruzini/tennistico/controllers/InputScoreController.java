556dc3a43cba3bcbb47f3f2b910f5eb737c1893e
lumat <lumat@op.pl>
2020-06-14 22:53:10 +0200

    create InputScoreController.java

--- src/main/java/com/gruzini/tennistico/controllers/InputScoreController.java
+++ src/main/java/com/gruzini/tennistico/controllers/InputScoreController.java
@@ -1,10 +1,28 @@
 package com.gruzini.tennistico.controllers;
 
+import com.gruzini.tennistico.models.score.Score;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/input-score")
 public class InputScoreController {
+    @PostMapping
+    public String inputScore(final Long matchId,
+                             @Valid @ModelAttribute(name = "score") final Score score,
+                             final Errors errors,
+                             final Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("message", "Invalid score");
+            return "dashboard";
+        }
 
+        return "dashboard";
+    }
 }
