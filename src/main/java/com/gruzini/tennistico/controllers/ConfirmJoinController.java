ff7ba427f2e040b0ad3827b5382f1781cdea0846
Bartek <bart.matusz@gmail.com>
2020-06-15 00:36:05 +0200

    changed get to post and pathVariable to requestParam

--- src/main/java/com/gruzini/tennistico/controllers/ConfirmJoinController.java
+++ src/main/java/com/gruzini/tennistico/controllers/ConfirmJoinController.java
@@ -2,9 +2,7 @@ package com.gruzini.tennistico.controllers;
 
 import com.gruzini.tennistico.services.ConfirmJoinService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -18,8 +16,8 @@ public class ConfirmJoinController {
         this.confirmJoinService = confirmJoinService;
     }
 
-    @GetMapping("/{match_id}")
-    public String confirmGuestJoiningMatch(@PathVariable(name = "match_id") final Long matchId, final Principal principal) {
+    @PostMapping
+    public String confirmGuestJoiningMatch(@RequestParam(name = "match_id") final Long matchId, final Principal principal) {
         confirmJoinService.confirmJoin(matchId, principal.getName());
         //TODO wysłać notification do Guesta
         return "dashboard";
