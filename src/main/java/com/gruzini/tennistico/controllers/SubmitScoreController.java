248d46a0524995a10195169e6ad8257bbf068056
Grzegorz <serwacy2@gmail.com>
2020-06-16 17:20:10 +0200

    created layout of page (missing images)

--- src/main/java/com/gruzini/tennistico/controllers/SubmitScoreController.java
+++ src/main/java/com/gruzini/tennistico/controllers/SubmitScoreController.java
@@ -1,7 +1,9 @@
 package com.gruzini.tennistico.controllers;
 
+import com.gruzini.tennistico.domain.Match;
 import com.gruzini.tennistico.services.MatchService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +19,10 @@ public class SubmitScoreController {
    }
 
    @GetMapping("/{match_id}")
-   public String showSubmitScorePage(@PathVariable(name = "match_id")final Long matchId){
-      matchService.getById(matchId);
+   public String showSubmitScorePage(@PathVariable(name = "match_id")final Long matchId,
+                                     final Model model){
+      final Match match = matchService.getById(matchId);
+      model.addAttribute("match", match);
       return "score";
    }
 }
