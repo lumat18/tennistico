e519606155c0d2b897202ef6ca0cd54c16b47fe1
lumat <lumat@op.pl>
2020-06-16 21:19:43 +0200

    add accumulate win/loss to RankingService

--- src/main/java/com/gruzini/tennistico/services/RankingService.java
+++ src/main/java/com/gruzini/tennistico/services/RankingService.java
@@ -2,13 +2,16 @@ package com.gruzini.tennistico.services;
 
 import com.gruzini.tennistico.domain.Match;
 import com.gruzini.tennistico.domain.Player;
+import com.gruzini.tennistico.domain.Ranking;
 import com.gruzini.tennistico.models.score.WinDecider;
 
 public class RankingService {
 
+    private final PlayerService playerService;
     private final WinDecider winDecider;
 
-    public RankingService(WinDecider winDecider) {
+    public RankingService(PlayerService playerService, WinDecider winDecider) {
+        this.playerService = playerService;
         this.winDecider = winDecider;
     }
 
@@ -21,6 +24,24 @@ public class RankingService {
      */
 
     public void updateRanking(final Match match) {
+        accumulateWins(match);
+        accumulateLosses(match);
+
+        //calculate ranking points
+
+
+        match.getPlayers().forEach(playerService::save);
+    }
+
+    private void accumulateWins(Match match) {
         final Player winner = winDecider.decideWinner(match);
+        final Ranking winnerRanking = winner.getRanking();
+        winnerRanking.setWins(winnerRanking.getWins() + 1);
+    }
+
+    private void accumulateLosses(Match match) {
+        final Player loser = winDecider.decideLoser(match);
+        final Ranking loserRanking = loser.getRanking();
+        loserRanking.setLosses(loserRanking.getLosses() + 1);
     }
 }
