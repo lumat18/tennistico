ffb9f9f84312dee4f29118618357f5f4113b9bcb
Grzegorz <serwacy2@gmail.com>
2020-06-16 16:29:44 +0200

    fixed some wierd git formatting

--- src/main/java/com/gruzini/tennistico/controllers/SubmitScoreController.java
+++ src/main/java/com/gruzini/tennistico/controllers/SubmitScoreController.java
@@ -1,21 +1,15 @@
 package com.gruzini.tennistico.controllers;
 
-<<<<<<< HEAD
 import com.gruzini.tennistico.services.MatchService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-=======
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> score-input
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/submit-score")
 public class SubmitScoreController {
 
-<<<<<<< HEAD
    private final MatchService matchService;
 
    public SubmitScoreController(final MatchService matchService) {
@@ -25,10 +19,6 @@ public class SubmitScoreController {
    @GetMapping("/{match_id}")
    public String showSubmitScorePage(@PathVariable(name = "match_id")final Long matchId){
       matchService.getById(matchId);
-=======
-   @GetMapping
-   public String showSubmitScorePage(){
->>>>>>> score-input
       return "score";
    }
 }
