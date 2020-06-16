232d4f03d9f59d91b86269d13355efd8277ef484
Bartek <bart.matusz@gmail.com>
2020-06-17 00:33:01 +0200

    connected Confirm Join, now it sends notification to match guest

--- src/main/java/com/gruzini/tennistico/services/ConfirmScoreService.java
+++ src/main/java/com/gruzini/tennistico/services/ConfirmScoreService.java
@@ -3,6 +3,7 @@ package com.gruzini.tennistico.services;
 import com.gruzini.tennistico.domain.Match;
 import com.gruzini.tennistico.domain.Player;
 import com.gruzini.tennistico.domain.enums.MatchStatus;
+import com.gruzini.tennistico.domain.enums.NotificationType;
 import com.gruzini.tennistico.exceptions.NoGuestInMatchException;
 import com.gruzini.tennistico.exceptions.PlayerIsNotMatchGuestException;
 import com.gruzini.tennistico.exceptions.WrongMatchStatusException;
@@ -10,37 +11,46 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ConfirmScoreService {
-   private final PlayerService playerService;
-   private final MatchService matchService;
+    private final PlayerService playerService;
+    private final MatchService matchService;
+    private final NotificationService notificationService;
+    private final UserService userService;
 
-   public ConfirmScoreService(final PlayerService playerService,
-                              final MatchService matchService) {
-      this.playerService = playerService;
-      this.matchService = matchService;
-   }
+    public ConfirmScoreService(final PlayerService playerService,
+                               final MatchService matchService, NotificationService notificationService, UserService userService) {
+        this.playerService = playerService;
+        this.matchService = matchService;
+        this.notificationService = notificationService;
+        this.userService = userService;
+    }
 
-   public void confirmScore(final Long matchId, final String username) {
-      final Player player = playerService.getByUsername(username);
-      final Match match = matchService.getById(matchId);
-      validateMatchAndPlayer(match, player);
-      matchService.updateMatchStatus(match, MatchStatus.ARCHIVED);
-   }
+    public void confirmScore(final Long matchId, final String username) {
+        final Player player = playerService.getByUsername(username);
+        final Match match = matchService.getById(matchId);
+        validateMatchAndPlayer(match, player);
+        matchService.updateMatchStatus(match, MatchStatus.ARCHIVED);
+        sendNotificationToMatchHost(matchId);
+    }
 
-   private void validateMatchAndPlayer(final Match match, final Player player) {
-      validateMatchStatus(match);
-      validateMatchGuest(match, player);
-   }
+    private void validateMatchAndPlayer(final Match match, final Player player) {
+        validateMatchStatus(match);
+        validateMatchGuest(match, player);
+    }
 
-   private void validateMatchStatus(final Match match) {
-      if(!match.getMatchStatus().equals(MatchStatus.SCORE_TO_BE_CONFIRMED)){
-         throw new WrongMatchStatusException();
-      }
-   }
+    private void validateMatchStatus(final Match match) {
+        if (!match.getMatchStatus().equals(MatchStatus.SCORE_TO_BE_CONFIRMED)) {
+            throw new WrongMatchStatusException();
+        }
+    }
 
-   private void validateMatchGuest(final Match match, final Player player) {
-      final Player guest = match.getGuest().orElseThrow(NoGuestInMatchException::new);
-      if(!guest.equals(player)){
-         throw new PlayerIsNotMatchGuestException();
-      }
-   }
+    private void validateMatchGuest(final Match match, final Player player) {
+        final Player guest = match.getGuest().orElseThrow(NoGuestInMatchException::new);
+        if (!guest.equals(player)) {
+            throw new PlayerIsNotMatchGuestException();
+        }
+    }
+
+    private void sendNotificationToMatchHost(Long matchId) {
+        notificationService.createNotificationFor(userService.getByPlayer(matchService.getById(matchId).getHost()), NotificationType.SCORE_TO_CONFIRM);
+    }
 }
