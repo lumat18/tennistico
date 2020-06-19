f1d976e5b4c3a6645d728af16a5d89282a93222a
Bartek <bart.matusz@gmail.com>
2020-06-19 02:14:33 +0200

    changes to repository methods
    in need of testing

--- src/main/java/com/gruzini/tennistico/repositories/MatchRepository.java
+++ src/main/java/com/gruzini/tennistico/repositories/MatchRepository.java
@@ -4,6 +4,8 @@ import com.gruzini.tennistico.domain.Match;
 import com.gruzini.tennistico.domain.Player;
 import com.gruzini.tennistico.domain.enums.MatchStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,11 +17,16 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
 
     List<Match> findByEndingAtBeforeAndMatchStatus(LocalDateTime endingDateTime, MatchStatus matchStatus);
 
-    List<Match> findAllByMatchStatusAndPlayersNotContainsAndStartingAtIsAfterOrderByStartingAt(MatchStatus status, Player player, LocalDateTime now);
+    //metoda wyciaga mecze po statusie wg playera ktorego w meczu nie ma
+    List<Match> findAllByMatchStatusAndAndHostNotAndStartingAtAfterOrderByStartingAt(MatchStatus status, Player player, LocalDateTime now);
+    //List<Match> findAllByMatchStatusAndPlayersNotContainsAndStartingAtIsAfterOrderByStartingAt(MatchStatus status, Player player, LocalDateTime now);
 
-    List<Match> findAllByPlayersContainsAndStartingAtIsAfterAndMatchStatusInOrderByStartingAt(Player player, LocalDateTime now, List<MatchStatus> matchStatusList);
+    //metoda wyciaga mecze playera
+    List<Match> findAllByHostOrGuestAndStartingAtAfterAndMatchStatusInOrderByStartingAt(Player player, Player player1, LocalDateTime now, List<MatchStatus> matchStatusList);
+    //List<Match> findAllByPlayersContainsAndStartingAtIsAfterAndMatchStatusInOrderByStartingAt(Player player, LocalDateTime now, List<MatchStatus> matchStatusList);
 
-    List<Match> getAllByPlayersAndMatchStatus(Player player, MatchStatus matchStatus);
+    @Query("SELECT m FROM matches m WHERE m.host=:pl OR m.guest=:pl AND m.matchStatus=:status")
+    List<Match> getAllByPlayerAndMatchStatus(@Param("pl") Player player, @Param("status") MatchStatus matchStatus);
 
     Optional<Match> findById(Long id);
 }
