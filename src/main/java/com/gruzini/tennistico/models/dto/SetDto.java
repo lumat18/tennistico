a132fe98298238638fdc929c7f4e593ba70281a5
Grzegorz <serwacy2@gmail.com>
2020-06-19 23:55:15 +0200

    revert changes to SetDto class

--- src/main/java/com/gruzini/tennistico/models/dto/SetDto.java
+++ src/main/java/com/gruzini/tennistico/models/dto/SetDto.java
@@ -7,39 +7,32 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Min;
 
-import static java.util.Objects.isNull;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SetDto {
-    @Min(0)
-    private int hostScore;
-    @Min(0)
-    private int guestScore;
+   @Min(0)
+   private int hostScore;
+   @Min(0)
+   private int guestScore;
 
-    @AssertTrue
-    public boolean isSetValid() {
-        if(isNull(hostScore) || isNull(guestScore)){
-            return true;
-        } else {
-            if(hostScore < 0 || guestScore < 0){
-                return false;
-            }
-            //normal scenario
-            if (hostScore == 6 && guestScore <= 4) {
-                return true;
-            }
-            if (guestScore == 6 && hostScore <= 4) {
-                return true;
-            }
-            //tiebreak scenario
-            if (hostScore == 7 && guestScore == 6 || hostScore == 6 && guestScore == 7) {
-                return true;
-            }
-            //advantage scenario
-            return hostScore == 7 && guestScore == 5 || hostScore == 5 && guestScore == 7;
-            //other scenarios are invalid
-        }
-    }
+   @AssertTrue
+   public boolean isSetValid() {
+
+      //normal scenario
+      if (hostScore == 6 && guestScore <= 4) {
+         return true;
+      }
+      if (guestScore == 6 && hostScore <= 4) {
+         return true;
+      }
+      //tiebreak scenario
+      if (hostScore == 7 && guestScore == 6 || hostScore == 6 && guestScore == 7) {
+         return true;
+      }
+      //advantage scenario
+      return hostScore == 7 && guestScore == 5 || hostScore == 5 && guestScore == 7;
+      //other scenarios are invalid
+        
+   }
 }
