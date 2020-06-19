826baddce80d6de12c1344b85d83534a75066316
Grzegorz <serwacy2@gmail.com>
2020-06-19 18:01:33 +0200

    corrected styling of score template

--- src/main/java/com/gruzini/tennistico/controllers/InputScoreController.java
+++ src/main/java/com/gruzini/tennistico/controllers/InputScoreController.java
@@ -43,6 +43,7 @@ public class InputScoreController {
       if (errors.hasErrors()) {
          model.addAttribute("match", matchService.getById(matchId));
          model.addAttribute("score", new ScoreDto());
+         model.addAttribute("msg", "Invalid score input");
          return "score";
       }
 
