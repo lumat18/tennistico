8cc8cfc6af1f4d69ea82a5cd299ae9be1b374346
Grzegorz <serwacy2@gmail.com>
2020-06-23 13:37:04 +0200

    adding ranking points to player during registration

--- src/main/java/com/gruzini/tennistico/domain/enums/PlayerSkill.java
+++ src/main/java/com/gruzini/tennistico/domain/enums/PlayerSkill.java
@@ -1,5 +1,18 @@
 package com.gruzini.tennistico.domain.enums;
 
 public enum PlayerSkill {
-   BEGINNER, AMATEUR, ADVANCED, PROFESSIONAL
+   BEGINNER(1000),
+   AMATEUR(1200),
+   ADVANCED(1400),
+   PROFESSIONAL(1600);
+
+   private final Integer rankingPoints;
+
+   PlayerSkill(final Integer rankingPoints) {
+      this.rankingPoints = rankingPoints;
+   }
+
+   public Integer getPoints() {
+      return rankingPoints;
+   }
 }
