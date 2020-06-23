e46ba777d8f503d99dc99b5d6a149068bbdd58d3
Bartek <bart.matusz@gmail.com>
2020-06-24 01:31:35 +0200

    name is displayed on notification - refactor could take place in notificationMapper

--- src/main/java/com/gruzini/tennistico/mappers/NotificationMapper.java
+++ src/main/java/com/gruzini/tennistico/mappers/NotificationMapper.java
@@ -1,20 +1,46 @@
 package com.gruzini.tennistico.mappers;
 
+import com.gruzini.tennistico.domain.Match;
 import com.gruzini.tennistico.domain.Notification;
+import com.gruzini.tennistico.domain.Player;
+import com.gruzini.tennistico.exceptions.PlayerNotFoundException;
 import com.gruzini.tennistico.models.dto.NotificationDto;
+import com.gruzini.tennistico.services.entity_related.MatchService;
 import org.springframework.stereotype.Component;
+
+import static java.util.Objects.isNull;
 
 @Component
 public class NotificationMapper {
+    private final MatchService matchService;
+
+    public NotificationMapper(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
     public NotificationDto toNotificationDto(Notification notification) {
         return NotificationDto.builder()
                 .id(notification.getId())
                 .createdAt(notification.getCreatedAt())
                 .endpoint(notification.getNotificationType().getEndpoint())
-                .message(notification.getNotificationType().getMessage())
+                .message(String.format(notification.getNotificationType().getMessage(), getOpponentName(notification, notification.getRecipient().getPlayer())))
                 .matchId(notification.getMatchId())
                 .buttonName(notification.getNotificationType().getButtonName())
                 .clicked(notification.isClicked())
                 .build();
+    }
+    private String getOpponentName(final Notification notification, final Player player){
+        final Long matchId = notification.getMatchId();
+        if (isNull(matchId)){
+            return "";
+        }
+        final Match match = matchService.getById(matchId);
+        Player opponent;
+        if (match.getHost().equals(player)){
+            opponent = match.getGuest().orElseThrow(PlayerNotFoundException::new);
+        }else {
+            opponent = match.getHost();
+        }
+        return opponent.getFirstName() + " " + opponent.getLastName();
     }
 }
