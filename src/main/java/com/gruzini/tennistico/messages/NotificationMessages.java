659e402f351972fce303b6df4b545514b8e44601
Bartek <bart.matusz@gmail.com>
2020-08-03 00:33:00 +0200

    added opponent name to fill score notification

--- src/main/java/com/gruzini/tennistico/messages/NotificationMessages.java
+++ src/main/java/com/gruzini/tennistico/messages/NotificationMessages.java
@@ -4,7 +4,7 @@ public class NotificationMessages {
     public static final String MATCH_CREATED = "You have created an open match!";
     public static final String JOIN_REQUEST = "%s made a request to join your game!";
     public static final String JOIN_CONFIRMED = "%s has confirmed your join request. Match is set up. Good luck!";
-    public static final String SCORE_TO_SUBMIT = "Please submit score of your last game. Click and fill in";
+    public static final String SCORE_TO_SUBMIT = "Your match vs %s took place recently. Please submit score. Click and fill in";
     public static final String SCORE_TO_CONFIRM = "%s have submitted the score of the match";
     public static final String MATCH_ARCHIVED = "Match has been archived. Check your ranking points!";
 
