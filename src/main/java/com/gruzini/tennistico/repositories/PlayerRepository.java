44385d707e7fa4212dfcbd29ea0581f1f809fd3d
Grzegorz <serwacy2@gmail.com>
2020-08-03 14:21:01 +0200

    add methods in player repository and services

--- src/main/java/com/gruzini/tennistico/repositories/PlayerRepository.java
+++ src/main/java/com/gruzini/tennistico/repositories/PlayerRepository.java
@@ -1,6 +1,8 @@
 package com.gruzini.tennistico.repositories;
 
 import com.gruzini.tennistico.domain.Player;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +11,6 @@ import java.util.Optional;
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     @Query("SELECT p FROM players p JOIN users u ON u.player.id=p.id WHERE u.email=:username")
     Optional<Player> findByUsername(final String username);
+
+    Page<Player> findAll(Pageable pageable);
 }
--- src/main/java/com/gruzini/tennistico/services/dto_related/PlayerDtoService.java
+++ src/main/java/com/gruzini/tennistico/services/dto_related/PlayerDtoService.java
@@ -3,23 +3,46 @@ package com.gruzini.tennistico.services.dto_related;
 import com.gruzini.tennistico.domain.Player;
 import com.gruzini.tennistico.mappers.PlayerDtoMapper;
 import com.gruzini.tennistico.models.dto.PlayerDto;
+import com.gruzini.tennistico.services.entity_related.PlayerService;
 import com.gruzini.tennistico.services.entity_related.UserService;
+import org.springframework.cglib.core.Converter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class PlayerDtoService {
    private final UserService userService;
+   private final PlayerService playerService;
    private final PlayerDtoMapper playerDtoMapper;
 
-   public PlayerDtoService(final UserService userService, final PlayerDtoMapper playerDtoMapper) {
+   public PlayerDtoService(final UserService userService,
+                           final PlayerService playerService,
+                           final PlayerDtoMapper playerDtoMapper) {
       this.userService = userService;
+      this.playerService = playerService;
       this.playerDtoMapper = playerDtoMapper;
    }
 
    public PlayerDto getPlayerDtoByEmail(final String email){
       final Player player = userService.getByEmail(email).getPlayer();
       return playerDtoMapper.toPlayerDto(player);
+   }
+
+   public Page<PlayerDto> getPlayerDtosPage(final Pageable pageable){
+      final Page<Player> playersPage = playerService.getPlayersPage(pageable);
+      return playersPage.map(new Function<Player, PlayerDto> () {
+         @Override
+         public PlayerDto apply(Player player){
+            PlayerDto playerDto = new PlayerDto();
+            playerDto = playerDtoMapper.toPlayerDto(player);
+            return playerDto;
+         }
+      });
    }
 }
