598fafd7fab443889de763813c54ae444d05f806
Grzegorz <serwacy2@gmail.com>
2020-08-09 16:13:03 +0200

    move cron expressions to properties in schedulers

--- src/main/java/com/gruzini/tennistico/scheduler/MatchStatusChangePublisher.java
+++ src/main/java/com/gruzini/tennistico/scheduler/MatchStatusChangePublisher.java
@@ -4,6 +4,7 @@ import com.gruzini.tennistico.domain.enums.MatchStatus;
 import com.gruzini.tennistico.events.ChangeMatchStatusByEndingDateTimeEvent;
 import com.gruzini.tennistico.events.ChangeMatchStatusByStartingDateTimeEvent;
 import com.gruzini.tennistico.events.HeldMatchEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,33 +18,34 @@ public class MatchStatusChangePublisher {
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
-    @Scheduled(cron = "0 0/1 * * * *")
+    //TODO: discuss scheduling for this event, for now used cron.upcoming
+    @Scheduled(cron = "${scheduling.cron.upcoming}")
     public void publishHeldGameEvent() {
         applicationEventPublisher.publishEvent(new HeldMatchEvent(this));
     }
 
-    @Scheduled(cron = "0 0/15 * * * *")
+    @Scheduled(cron = "${scheduling.cron.upcoming}")
     public void changeStatusFromJoinRequestToBusted() {
         final ChangeMatchStatusByStartingDateTimeEvent event =
                 new ChangeMatchStatusByStartingDateTimeEvent(this, MatchStatus.JOIN_REQUEST, MatchStatus.BUSTED);
         applicationEventPublisher.publishEvent(event);
     }
 
-    @Scheduled(cron = "0 0/15 * * * *")
+    @Scheduled(cron = "${scheduling.cron.upcoming}")
     public void changeStatusFromHostedToBusted() {
         final ChangeMatchStatusByStartingDateTimeEvent event =
                 new ChangeMatchStatusByStartingDateTimeEvent(this, MatchStatus.HOSTED, MatchStatus.BUSTED);
         applicationEventPublisher.publishEvent(event);
     }
 
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "${scheduling.cron.pending}")
     public void changeStatusFromScoreToBeConfirmedToBusted() {
         final ChangeMatchStatusByEndingDateTimeEvent event =
                 new ChangeMatchStatusByEndingDateTimeEvent(this, MatchStatus.SCORE_TO_BE_CONFIRMED, MatchStatus.BUSTED);
         applicationEventPublisher.publishEvent(event);
     }
 
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "${scheduling.cron.pending}")
     public void changeStatusFromPendingToBusted() {
         final ChangeMatchStatusByEndingDateTimeEvent event =
                 new ChangeMatchStatusByEndingDateTimeEvent(this, MatchStatus.PENDING, MatchStatus.BUSTED);
